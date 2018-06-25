edd42b4c5be18bba3d6a2aeb9d4124a3a04fa338
ThienT <thient@vng.com.vn>
2018-06-25 17:26:45 +0700

    add init script for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: rake db:setup setup:generate_teams
 web: bundle exec puma -C config/puma.rb
 clock: bundle exec clockwork lib/new_scraper_clock.rb
--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -4,8 +4,8 @@ Airbrake.configure do |c|
   # c.project_key = ENV['AIRBRAKE_KEY']
   # c.root_directory = Rails.root
   # c.logger = Rails.logger
-  # c.environment = Rails.env
-  # c.ignore_environments = %w[test development staging]
+  c.environment = "Rails.env"
+  c.ignore_environments = %w[test development staging production]
   # c.blacklist_keys = [/password/i, /authorization/i]
   c.project_id = 188559
   c.project_key = '7ce8e0bf38beb8d38a85ca5d68f41230'
